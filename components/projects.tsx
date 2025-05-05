5830ac6af863761f9f181ba17cb75d68c161a25b
Osamudiamen Nwoko <59775183+charles-55@users.noreply.github.com>
2025-05-05 00:13:57 -0600

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -32,7 +32,7 @@ export function Projects() {
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {projectsData.slice(0, visibleProjects).map((project, index) => (
             <Card key={index} className="overflow-hidden">
-              <div className="relative h-48 w-full">
+              <div className="relative pt-4 h-48 w-full">
                 <Image src={project.image || "/logo.png"} alt={project.title} fill className="object-contain" />
               </div>
               <CardContent className="p-6">
